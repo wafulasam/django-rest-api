66053142f51ea37701fac310ea4ead55d7fd56cb
Sammie <sammie@Sammies-MBP.mshome.net>
2021-06-24 16:09:07 +0300

    [feature]: urls

--- rest_api_demo/rest_api_demo/urls.py
+++ rest_api_demo/rest_api_demo/urls.py
@@ -16,6 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.urls import  include, path
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Setup automatic URL routing
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('/auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
