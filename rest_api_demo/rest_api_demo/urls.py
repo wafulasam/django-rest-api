566dd6898efc94e118b37cf59bd40bed7ebc63ee
Sammie <sammie@Sammies-MBP.mshome.net>
2021-06-24 16:11:42 +0300

    [feature]: pagination

--- rest_api_demo/rest_api_demo/urls.py
+++ rest_api_demo/rest_api_demo/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from django.urls import  include, path
+from django.urls import include, path
 from rest_framework import routers
 from api import views
 
